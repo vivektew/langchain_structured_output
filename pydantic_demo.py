7b372830d0a590c8ba272a94a1c920ca6639ba3c
Vivek Tiwari <vivek.sd10@gmail.com>
2025-09-16 22:32:50 +0530

    Initial commit

---
+++ pydantic_demo.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+class Student(BaseModel):
+    name: str = 'Vivek'
+    age: Optional[int] = None
+    email: EmailStr
+    cgpa: float = Field(gt=0, lt=10, default=5, description='A decimal value representing the cgpa of the student')
+
+new_student = {'age':34, 'email':'abc@gmail.com'}
+
+student = Student(**new_student)
+
+student_dict = dict(student)
+
+print(student_dict['age'])
+
+student_json = student.model_dump_json()
+
+print(student_json)
+
+
+
+
